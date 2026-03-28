b38f79967f8a9ca3b0dd47b9533213efcd8af2f7
Thomaszdravko <thomaskulich@hotmail.com>
2026-03-28 21:10:57 +1000

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "fungi-field-notes-v1";
+
+const APP_SHELL = [
+  "./",
+  "./fieldnotes.html",
+  "./manifest.json",
+  "./SIX_9DEC9DD4-4378-4975-90D3-A9DCBFA380E8.png",
+  "./SIX_23C2F4BF-4216-4448-9422-E561138A1CF5.png",
+  "./SIX_FAF197CF-C703-4DE5-99F2-D27EA2A569B1.png",
+  "./0027015A-CFA3-4237-AD58-B7418E9B049E.jpeg",
+  "./IMG_1678.png",
+  "./IMG_0238.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
